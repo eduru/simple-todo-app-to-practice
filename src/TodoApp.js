e3195e028ca331eec2cb0f83551b3d372d6c5b48
Edur <eduru@me.com>
2021-02-25 14:05:49 +0900

    installing material ui and creating the TodoApp component

---
+++ src/TodoApp.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import AppBar from "@material-ui/core/AppBar";
+import ToolBar from "@material-ui/core/Toolbar";
+import Grid from "@material-ui/core/Grid";
+
+function TodoApp() {
+  return (
+    <Paper
+      style={{
+        padding: 0,
+        margin: 0,
+        height: "100vh",
+        backgroundColor: "#fafafa",
+      }}
+      elevation={0}
+    >
+      <AppBar color="primary" posotion="static" style={{ height: "64px" }}>
+        <ToolBar>
+          <Typography color="inherit">TODO APP</Typography>
+        </ToolBar>
+      </AppBar>
+    </Paper>
+  );
+}
+
+export default TodoApp;
